2ec2b16d12c790f673eaa79b273f2b876386bd2c
torruria <166187771+torruria@users.noreply.github.com>
2024-05-18 00:56:19 -0400

    mid.r

--- mid.r
+++ mid.r
@@ -1,25 +1,49 @@
-# Linear regression plot
-linear_predicted <- predict(linear_model)
-plot(iris_data$SepalWidthCm, linear_predicted, main = "Linear Regression: Actual vs. Predicted", xlab = "Actual Sepal Width (cm)", ylab = "Predicted Sepal Width (cm)")
-abline(0, 1, col = "red")  # Add a diagonal line for reference
-
-# Polynomial regression plot
-polynomial_predicted <- predict(polynomial_model)
-plot(iris_data$SepalWidthCm, polynomial_predicted, main = "Polynomial Regression: Actual vs. Predicted", xlab = "Actual Sepal Width (cm)", ylab = "Predicted Sepal Width (cm)")
-abline(0, 1, col = "red")  # Add a diagonal line for reference
-
-# Logistic regression plot
-# First, we need to generate predicted probabilities
-logistic_predicted_prob <- predict(logistic_model, type = "response")
-
-# Now, let's create a confusion matrix plot
+# Load necessary libraries
+library(MASS)
 library(ggplot2)
-library(caret)
-confusion_matrix <- confusionMatrix(ifelse(logistic_predicted_prob > 0.5, 1, 0), iris_data$Species_binary)
 
-# Plot confusion matrix
-ggplot(data = as.data.frame(confusion_matrix$table), aes(x = Reference, y = Prediction, fill = Freq)) +
-  geom_tile() +
-  geom_text(aes(label = Freq), vjust = 1) +
-  scale_fill_gradient(low = "white", high = "blue") +
-  labs(title = "Logistic Regression: Confusion Matrix", x = "Predicted", y = "Actual")
+#linear Reggression
+# Load the Boston dataset
+data("Boston")
+
+# Fit the linear regression model
+linear_model <- lm(medv ~ rm, data = Boston)
+
+# Summary of the model
+summary(linear_model)
+
+# Plot the data and the regression line
+ggplot(Boston, aes(x = rm, y = medv)) +
+  geom_point() +
+  geom_smooth(method = "lm", col = "blue") +
+  labs(title = "Linear Regression: Median Value vs. Rooms", x = "Average Number of Rooms (rm)", y = "Median Value of Homes (medv)")
+
+# Ploynomial
+# Fit the polynomial regression model
+poly_model <- lm(medv ~ poly(rm, 2), data = Boston)
+
+# Summary of the model
+summary(poly_model)
+
+# Plot the data and the polynomial regression line
+ggplot(Boston, aes(x = rm, y = medv)) +
+  geom_point() +
+  geom_smooth(method = "lm", formula = y ~ poly(x, 2), col = "red") +
+  labs(title = "Polynomial Regression: Median Value vs. Rooms", x = "Average Number of Rooms (rm)", y = "Median Value of Homes (medv)")
+
+
+#logistic
+# Create a binary outcome variable
+Boston$medv_binary <- ifelse(Boston$medv > 25, 1, 0)
+
+# Fit the logistic regression model
+logistic_model <- glm(medv_binary ~ rm, data = Boston, family = binomial)
+
+# Summary of the model
+summary(logistic_model)
+
+# Plot the data and the logistic regression curve
+ggplot(Boston, aes(x = rm, y = medv_binary)) +
+  geom_point(alpha = 0.5) +
+  stat_smooth(method = "glm", method.args = list(family = "binomial"), se = FALSE, col = "green") +
+  labs(title = "Logistic Regression: High/Low Median Value vs. Rooms", x = "Average Number of Rooms (rm)", y = "High Median Value (medv_binary)")
